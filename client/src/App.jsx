5ab1fb65f65460c865e43b79010d096c16888857
SUNGJUN1234 <chotjdwns654@naver.com>
2024-06-16 15:56:11 +0900

    threejs 테스트

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ServerStatus from './components/ServerStatus';
+import Test from './components/Test';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <ServerStatus />
+      {/* <ServerStatus /> */}
+      <Test />
     </div>
   );
 }

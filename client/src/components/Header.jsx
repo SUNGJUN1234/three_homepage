161c255f588338f0f2828b32f2a7361085a4dc91
SUNGJUN1234 <chotjdwns654@naver.com>
2024-06-18 20:26:51 +0900

    헤더 생성

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
+
+const headerNav = [
+    {
+        title: "intro",
+        url: "#intro"
+    },
+    {
+        title: "news",
+        url: "#news"
+    }
+];
 
 const Header = () => {
-  return (
-    <div>
-      header
-    </div>
-  )
-}
+    const [show, setShow] = useState(false);
 
-export default Header
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow);
+    };
+
+    return (
+        <header id="header" role="banner">
+            <div className="header__inner">
+                <div className="header__logo">
+                    <a href="/">Homepage<em>three.js</em></a>
+                </div>
+                <nav className={`header__nav ${show ? "show" : ""}`} role="navigation" aria-label="메인 메뉴">
+                    <ul>
+                        {headerNav.map((nav, key) => (
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <div
+                    className="header__nav__mobile"
+                    id="headerToggle"
+                    aria-controls="primary-menu"
+                    aria-expanded={show ? "true" : "false"}
+                    role="button" 
+                    tabIndex="0"
+                    onClick={toggleMenu}
+                >
+                    <span></span>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;

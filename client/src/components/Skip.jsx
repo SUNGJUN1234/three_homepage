bc45cd03e952adaf3cadbe06ee641a10eebb715f
SUNGJUN1234 <chotjdwns654@naver.com>
2024-06-18 20:58:25 +0900

    tab skip 영역 생성

--- client/src/components/Skip.jsx
+++ client/src/components/Skip.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Skip = () => {
   return (
-    <div>
-      skip
+    <div id='skip'>
+      <a href='#header'>헤더 영역 바로가기</a>
+      <a href='#intro'>소개 영역 바로가기</a>
+      <a href='#news'>뉴스 영역 바로가기</a>
+      <a href='#footer'>푸터 영역 바로가기</a>
     </div>
   )
 }

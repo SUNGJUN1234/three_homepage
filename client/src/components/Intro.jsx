837d2c84e9ffe28ecfe9f796300877fc5e6e156f
SUNGJUN1234 <chotjdwns654@naver.com>
2024-06-19 01:02:43 +0900

    인트로 페이지

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+
+import about from "../assets/img/about.png";
 
 const Intro = () => {
-  return (
-    <div>
-      intro
-    </div>
-  )
-}
+    return (
+        <section id="intro">
+            <div className="intro__inner">
+                <h1 className="intro__title">3D Homepage</h1>
+                <div className="intro__lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+                <div className="intro__text">
+                    <div className="text">
+                        <div>Geospatial Company</div>
+                        <div>for</div>
+                        <div>Digital Twin</div>
+                    </div>
+                    <div className="img">
+                        <img src={about} alt="어바웃" />
+                    </div>
+                </div>
+                <div className="intro__lines bottom" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+            </div>
+        </section>
+    );
+};
 
-export default Intro
+export default Intro;

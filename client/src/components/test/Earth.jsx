1e6b257b090540dc45aa9385767da1557aa27120
SUNGJUN1234 <chotjdwns654@naver.com>
2024-06-26 23:51:35 +0900

    Earth 컴포넌트 백스라운드로 설정

--- client/src/components/test/Earth.jsx
+++ client/src/components/test/Earth.jsx
@@ -168,11 +168,12 @@ const Earth = ({ children }) => {
     }, []);
 
     return (
-        <div ref={mountRef} style={{ position: 'relative', width: '100vw', height: '100vh' }}>
-            <div key={location.key} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
+        <>
+            <div className="earth" ref={mountRef} style={{display: 'fixed', zIndex:0}} ></div>
+            <div key={location.key} >
                 {children}
             </div>
-        </div>
+        </>
     );
 };
 

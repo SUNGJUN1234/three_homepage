b0280a09eff277e5f983bd31fdfb8ba2a5b8f1eb
SUNGJUN1234 <chotjdwns654@naver.com>
2024-06-26 23:39:25 +0900

    lenis, link 구현

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeView from './views/HomeView';
 import AboutView from './views/AboutView';
 import Earth from './components/test/Earth'
+import lenis from "./utils/lenis";
+import link from "./utils/link";
 
+const App = () => {
+  useEffect(() => {
+      lenis();
+      link();
+  }, []);
 
-function App() {
   return (
   <BrowserRouter>
     <Earth >
